308a78780bd9fee2cad0d181d6b5c48d46548955
Leonte Ionut-Claudiu <leonteionut98@yahoo.com>
2022-11-28 19:57:36 +0200

    Update project v2

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,35 +1,49 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import classes from './Navbar.module.css';
 
 function Navigation() {
   return (
-    <Navbar bg="light" expand="lg" sticky="top">
-      <Container>
-        <Navbar.Brand href="#home">Aqviser logo</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Tutorials</Nav.Link>
-            <Nav.Link href="#link">Help</Nav.Link>
-            <Nav.Link href="#link">FAQ</Nav.Link>
-            <Nav.Link href="#link">Contact</Nav.Link>
-            <Nav.Link href="#link">Login</Nav.Link>
-            {/* <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown> */}
-          </Nav>
-        </Navbar.Collapse>
+    <Navbar className={classes.navbar} expand="lg" sticky="top">
+      <Container className={classes.navigation}>
+        <div>
+          <Navbar.Brand href="#home">
+            <img
+              className={classes.logo}
+              src="https://i.ibb.co/wr62C4q/Aqmeter-Logo-Small-White.png"
+              alt="Logo"
+            />
+          </Navbar.Brand>
+        </div>
+        <div>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link className={classes.navLink} href="#home">
+                Home
+              </Nav.Link>
+              <Nav.Link className={classes.navLink} href="#link">
+                Tutorials
+              </Nav.Link>
+              <Nav.Link className={classes.navLink} href="#link">
+                Help
+              </Nav.Link>
+              <Nav.Link className={classes.navLink} href="#link">
+                FAQ
+              </Nav.Link>
+              <Nav.Link className={classes.navLink} href="#link">
+                Contact
+              </Nav.Link>
+              <Nav.Link
+                className={(classes.navLink, classes.login)}
+                href="#link"
+              >
+                Login
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </div>
       </Container>
     </Navbar>
   );
